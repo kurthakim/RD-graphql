e462a1231e9b4765294a18785e319ed7dc6717e9
Kurt Hakim <kurthakim@gmail.com>
2022-04-07 13:24:06 +0700

    added  blank login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Nav from './components/Nav';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
 
 const App = () => {
   return (
     <>
       <Nav />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 };
