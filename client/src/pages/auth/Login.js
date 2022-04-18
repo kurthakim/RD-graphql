76b78421612da40f2e82d1e41f9570c869c7f69a
Kurt Hakim <kurthakim@gmail.com>
2022-04-18 11:07:45 +0700

    kerja tengah2

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,31 +1,48 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { AuthContext } from '../../context/authContext';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { auth, googleAuthProvider } from '../../firebase';
 
 const Login = () => {
+  const { dispatch } = useContext(AuthContext);
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = () => {
     // TODO
   };
 
   return (
-    <div className="container">
-      <div className="row p-5">
-        <h4>Login</h4>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>Email Address</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              placeholder="Enter email"
-              disabled={loading}
-            />
-          </div>
-        </form>
-      </div>
+    <div className="container p-5">
+      <h4>Login</h4>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+            placeholder="Enter email"
+            disabled={loading}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+            placeholder="Enter password"
+            disabled={loading}
+          />
+        </div>
+      </form>
     </div>
   );
 };
